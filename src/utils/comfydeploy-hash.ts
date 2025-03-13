0e8640553225aa8d392a98f4e1b0075d0afd445f
karrix <thomasdice1834793903@gmail.com>
2025-03-13 20:10:34 +0800

    revert comfy deploy hash

--- src/utils/comfydeploy-hash.ts
+++ src/utils/comfydeploy-hash.ts
@@ -1,3 +1,3 @@
-export const comfydeploy_hash = "6ecf9c782b92cb612f69d36f80081e7bd8c0d6b0";
+export const comfydeploy_hash = "ce3b0dbe849ed58285adfca49d42a7a1e3370589";
 
 export const comfyui_hash = "9aac21f894a122ddb8d825c57ad61c0db5e630db";
